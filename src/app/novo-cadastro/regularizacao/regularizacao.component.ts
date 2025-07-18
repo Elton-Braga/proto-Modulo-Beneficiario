af0f6bc1f3e4a1e051c4ad2201c05996dc16c7fc
Elton <elton.b.braga@gmail.com>
2025-07-18 09:28:03 -0300

    Inclcusão de campos condicionais na tela de regularização

--- src/app/novo-cadastro/regularizacao/regularizacao.component.ts
+++ src/app/novo-cadastro/regularizacao/regularizacao.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { ChangeDetectionStrategy, Component, model } from '@angular/core';
 import {
   FormBuilder,
   FormGroup,
@@ -6,10 +7,13 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 
 @Component({
@@ -21,6 +25,10 @@ import { MatSelectModule } from '@angular/material/select';
     FormsModule,
     MatDatepickerModule,
     ReactiveFormsModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatCardModule,
+    NgIf,
   ],
   providers: [provideNativeDateAdapter()],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -29,6 +37,10 @@ import { MatSelectModule } from '@angular/material/select';
 })
 export class RegularizacaoComponent {
   form: FormGroup;
+  readonly checked = model(false);
+  readonly indeterminate = model(false);
+  readonly labelPosition = model<'before' | 'after'>('after');
+  readonly disabled = model(false);
   tipo_regularizacao: any[] = [
     { value: 'tipo_0', viewValue: 'tipo 0' },
     { value: 'tipo_1', viewValue: 'tipo 1' },
@@ -47,12 +59,25 @@ export class RegularizacaoComponent {
     //{ value: 'Separação do Casal', viewValue: 'Separação do Casal' },
   ];
 
+  situacao_conjugal: any[] = [
+    { value: 'Pessoa Externa ao PNRA', viewValue: 'Pessoa Externa ao PNRA' },
+    { value: 'Beneficiario do PNRA', viewValue: 'Beneficiario do PNRA' },
+    //{ value: 'Separação do Casal', viewValue: 'Separação do Casal' },
+  ];
+
   constructor(fb: FormBuilder) {
     this.form = fb.group({
       tipo_regularizacao: [null, Validators.required],
       detalhe_regularizacao: [null, Validators.required],
       data_regularizacao: [null, Validators.required],
       inclusao_PNRA: [null, Validators.required],
+      inclusao_titular: [],
+      inclusao_unidade_familiar: [],
+      inclusao_assentamento: [],
+      cod_titular: [],
+      cpf_titular2: [],
+      data_uniao: [],
+      situacao_conjugal: [],
     });
   }
 
