7bbfe4aa9848106795db89ce0d89c7ee3f507637
Elton <elton.b.braga@gmail.com>
2025-07-24 16:45:35 -0300

    ajuste na tabela da modal

--- src/app/lista/modal/modal-espelho/modal-espelho.component.ts
+++ src/app/lista/modal/modal-espelho/modal-espelho.component.ts
@@ -14,7 +14,7 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 export class ModalEspelhoComponent {
   beneficiario: Beneficiario;
 
-  items = ['Beneficiario 1', 'Beneficiario 2'];
+  items = ['Beneficiarios', 'Dados do Projeto'];
   expandedIndex = 0;
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: Beneficiario) {
