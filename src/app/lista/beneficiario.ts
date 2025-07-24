c62d06a15c10aa6dd8f5e0dbe8183a20d251d24d
Elton <elton.b.braga@gmail.com>
2025-07-24 10:57:03 -0300

    melhorando a organização do componente lista

---
+++ src/app/lista/beneficiario.ts
@@ -0,0 +1,15 @@
+export interface Beneficiario {
+  check: any;
+  codigo_projeto: string;
+  projeto: any;
+  lote: number;
+  nome_T1: string;
+  situacao_T1: any;
+  nome_T2: string;
+  situacao_T2: any;
+  data_situacao_T2: any;
+  regularizacao: string;
+  periodo_regularizacao: string;
+  status: string;
+  acoes: string[];
+}
