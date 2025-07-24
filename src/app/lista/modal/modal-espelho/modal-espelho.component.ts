6da6b360a8b55a6db119ef1e0ba60e0f83dbbe04
Elton <elton.b.braga@gmail.com>
2025-07-24 11:39:30 -0300

    ajuste da modal Espelho

--- src/app/lista/modal/modal-espelho/modal-espelho.component.ts
+++ src/app/lista/modal/modal-espelho/modal-espelho.component.ts
@@ -1,11 +1,13 @@
+import { CdkAccordionModule } from '@angular/cdk/accordion';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-modal-espelho',
-  imports: [],
+  imports: [CdkAccordionModule],
   templateUrl: './modal-espelho.component.html',
-  styleUrl: './modal-espelho.component.scss'
+  styleUrl: './modal-espelho.component.scss',
 })
 export class ModalEspelhoComponent {
-
+  items = ['Dados do Titular 1', 'Dados do Titular 1'];
+  expandedIndex = 0;
 }
