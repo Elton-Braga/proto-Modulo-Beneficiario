be044589bf2435fa6ba6cf82f5c07e49e4d3e16d
Elton <elton.b.braga@gmail.com>
2025-08-07 13:34:11 -0300

    criado btn voltar

--- src/app/lista/modal/modal-regularizacao/modal-regularizacao.component.ts
+++ src/app/lista/modal/modal-regularizacao/modal-regularizacao.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Beneficiario } from '../../beneficiario';
 
 @Component({
@@ -9,9 +9,16 @@ import { Beneficiario } from '../../beneficiario';
   styleUrl: './modal-regularizacao.component.scss',
 })
 export class ModalRegularizacaoComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public dados: Beneficiario) {}
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public dados: Beneficiario,
+    private dialogRef: MatDialogRef<ModalRegularizacaoComponent>
+  ) {}
 
   ngOnInit() {
     console.log(this.dados); // Para conferência
+  }
+
+  fechar(): void {
+    this.dialogRef.close(); // Fecha a modal
   }
 }
