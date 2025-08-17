4750953fb9912605d0f8dfd2c803f669e6bfd182
Elton <elton.b.braga@gmail.com>
2025-08-17 19:13:22 -0300

    inserindo campos na tela de bloqueio

---
+++ src/app/lista/beneficiarioBloqueio.ts
@@ -0,0 +1,14 @@
+export interface BeneficiarioBloqueio {
+  codigoBeneficiario: string;
+  codigoTipoBloqueio: string;
+  descricaoBloqueio: string;
+  codigoTransacao: string;
+  dataBloqueio: string;
+  codigoSubBloqueio: string;
+  descricaoSubBloqueio: string;
+  codigoMotivoBloqueio: string;
+  descricaoMotivoBloqueio: string;
+  desbloqueioAtendido: string;
+  situacaoAnalise: string;
+  dataResultado: string;
+}
--- src/app/novo-cadastro/bloqueios/bloqueios.component.ts
+++ src/app/novo-cadastro/bloqueios/bloqueios.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-bloqueios',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+  ],
   templateUrl: './bloqueios.component.html',
   styleUrl: './bloqueios.component.scss',
   standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BloqueiosComponent {}
+export class BloqueiosComponent {
+  form!: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      codigoBeneficiario: ['', Validators.required],
+      codigoTipoBloqueio: ['', Validators.required],
+      descricaoBloqueio: ['', [Validators.required, Validators.minLength(3)]],
+      codigoTransacao: ['', Validators.required],
+      dataBloqueio: [
+        '',
+        [Validators.required, Validators.pattern(/^\d{4}-\d{2}-\d{2}$/)],
+      ],
+      codigoSubBloqueio: ['', Validators.required],
+      descricaoSubBloqueio: [
+        '',
+        [Validators.required, Validators.minLength(3)],
+      ],
+      codigoMotivoBloqueio: ['', Validators.required],
+      descricaoMotivoBloqueio: [
+        '',
+        [Validators.required, Validators.minLength(3)],
+      ],
+      desbloqueioAtendido: ['', Validators.required],
+      situacaoAnalise: ['', Validators.required],
+      dataResultado: [
+        '',
+        [Validators.required, Validators.pattern(/^\d{4}-\d{2}-\d{2}$/)],
+      ],
+    });
+  }
+}
