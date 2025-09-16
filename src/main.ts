8bebbcd7ad6f07310f04761543469176e626dc95
Elton <elton.b.braga@gmail.com>
2025-09-16 10:34:40 -0300

    ajustes nas rotas

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import { provideRouter } from '@angular/router';
 import { APP_BASE_HREF } from '@angular/common';
 import { routes } from './app/app.routes';
 import { environment } from './environments/environment'; // <-- importa o ambiente correto
+import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: APP_BASE_HREF, useValue: '/proto-Modulo-Beneficiario/' },
     provideRouter(routes),
+    provideHttpClient(),
   ],
 });
