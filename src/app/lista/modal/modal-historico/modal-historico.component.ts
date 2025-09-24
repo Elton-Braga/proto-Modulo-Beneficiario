89ba7b162a9ef4d272469d5c95a91d54739a18c3
Elton <elton.b.braga@gmail.com>
2025-09-24 11:29:24 -0300

    ajuste

--- src/app/lista/modal/modal-historico/modal-historico.component.ts
+++ src/app/lista/modal/modal-historico/modal-historico.component.ts
@@ -11,5 +11,22 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class ModalHistoricoComponent {
   @Input() historicoPNRA: HistoricoPNRA[] = [];
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {
+    if (data?.historicoPNRA) {
+      this.historicoPNRA = this.removerDuplicatas(data.historicoPNRA);
+    }
+  }
+
+  private removerDuplicatas(historico: HistoricoPNRA[]): HistoricoPNRA[] {
+    const mapa = new Map<string, HistoricoPNRA>();
+
+    historico.forEach((item) => {
+      const chave = `${item.codigo_beneficiario}-${item.nome_PA}-${item.situacao}-${item.data_Situacao}`;
+      if (!mapa.has(chave)) {
+        mapa.set(chave, item);
+      }
+    });
+
+    return Array.from(mapa.values());
+  }
 }
